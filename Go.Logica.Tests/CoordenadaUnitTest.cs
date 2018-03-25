b094038ba8d50daea61f4d67e81cb08e148ac146
Augusto Romero <augromara@gmail.com>
2018-03-24 19:19:51 -0500

    Coordenada

---
+++ Go.Logica.Tests/CoordenadaUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Go.Logica.Tests
+{
+    [TestClass]
+    public class CoordenadaUnitTest
+    {
+        Coordenada _coordenada;
+
+        [TestInitialize]
+        public void Inicializar()
+        {
+            _coordenada = new Coordenada();
+        }
+
+        [TestMethod]
+        public void CuandoIngresa_X1Y1_RetornaA1()
+        {
+
+            int x = 1;
+            int y = 1;
+
+            string literalCoordenada = _coordenada.ObtenerLiteral(x, y);
+
+            Assert.AreEqual("A1", literalCoordenada);
+        }
+    }
+}
---
+++ Go.Logica/Coordenada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Go.Logica
+{
+    public class Coordenada
+    {
+        const int unicodeAntesDeLetraA = 64;
+        public string ObtenerLiteral(int x, int y)
+        {
+            int unicodeCoordenadaX = unicodeAntesDeLetraA + x;
+            string literalCoordenadaX = ((char)unicodeCoordenadaX).ToString();
+
+            return literalCoordenadaX + y;
+        }
+    }
+}
