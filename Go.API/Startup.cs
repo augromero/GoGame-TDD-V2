28b8b2dba0325f13932fc495982291121a973854
AUGUSTO ROMERO ARANGO <augustoromeroarango@192.168.0.5>
2018-03-26 23:35:16 -0500

    Configuración de CoreContext

--- Go.API/Startup.cs
+++ Go.API/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Go.Repositorio;
+using Microsoft.EntityFrameworkCore;
 
 namespace Go.API
 {
@@ -23,6 +25,10 @@ namespace Go.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CoreContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddMvc();
         }
 
---
+++ Go.Repositorio/CoreContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Go.Entidades;
+
+namespace Go.Repositorio
+{
+    public class CoreContext : DbContext
+    {
+        public CoreContext(DbContextOptions<CoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Punto> Puntos { get; set; }
+
+    }
+}
