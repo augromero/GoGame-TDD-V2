61810ac37c2cd42125eae85abf305bb85463f2e5
Augusto Romero <augromara@gmail.com>
2018-04-06 19:16:16 -0500

    Nuevo proyecto de tests con NUnit

---
+++ Go.Servicios.Tests/PartidaServicioUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Go.Servicios.Tests
+{
+    [TestFixture]
+    public class PartidaServicioUnitTest
+    {
+        [Test]
+        public void CuandoIngresaTamaño()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
